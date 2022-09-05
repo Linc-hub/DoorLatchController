d7d7ff81e59ee1c87b76912b21a2886214908f27
Lincoln Runco <lincolndrunco@gmail.com>
2022-09-05 00:43:05 -0400

    Added IR Remote Support

--- Door_Latch.ino
+++ Door_Latch.ino
@@ -1,3 +1,4 @@
+#include <IRremote.h>
 #include <Servo.h>
 Servo myservo;
 
@@ -6,38 +7,73 @@ int CLOSED_POSITION = 100;
 int SERVO_PIN = 2;
 int BUTTON_PIN = 7;
 int LED_PIN = 13;
+int IR_PIN = 12;
+int IR_OPEN_CODE = 16;
+int IR_CLOSED_CODE = 18;
 
 int currentButtonState;
 int lastButtonState;
+
+IRrecv irrecv(IR_PIN);
+IRData results;
 
 void setup() 
 {
   myservo.attach(SERVO_PIN);
   pinMode(BUTTON_PIN, INPUT);
   pinMode(LED_PIN, OUTPUT);
+  pinMode(IR_PIN, INPUT);
+  irrecv.enableIRIn();
   Serial.begin(9600);
+}
+
+void openLatch()
+{
+  myservo.write(OPEN_POSITION);
+  digitalWrite(LED_PIN, LOW);
+  Serial.println("Open Latch");
+}
+
+void closeLatch()
+{
+  myservo.write(CLOSED_POSITION);
+  digitalWrite(LED_PIN, HIGH);
+  Serial.println("Close Latch");
 }
 
 void loop() 
 {
+  boolean hasReceivedData = irrecv.decode();
   currentButtonState = digitalRead(BUTTON_PIN);
   
   if (currentButtonState != lastButtonState) 
   {
     if (currentButtonState == LOW) 
     {
-      myservo.write(OPEN_POSITION);
-      digitalWrite(LED_PIN, LOW);
-      Serial.println("digitalWrite(LOW)");
+        openLatch();
     }
-
-    if (currentButtonState == HIGH) 
+    else if (currentButtonState == HIGH) 
     {
-      myservo.write(CLOSED_POSITION);
-      digitalWrite(LED_PIN, HIGH);
-      Serial.println("digitalWrite(HIGH)");
+        closeLatch();
     }
 
     lastButtonState = currentButtonState;
+  }
+  else if(hasReceivedData)
+  {
+    irrecv.resume();
+    results = irrecv.decodedIRData;
+
+
+    Serial.println(results.command);
+
+    if (results.command == IR_OPEN_CODE)
+    {
+       openLatch();
+    }
+    else if(results.command == IR_CLOSED_CODE)
+    {
+       closeLatch();
+    }    
   }
 }
